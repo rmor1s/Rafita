a1fa464c28d5d24517facf7cec7c21157b95fca3
rmor1s <richard.mor12@gmail.com>
2018-09-22 13:05:40 -0300

    fin1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,11 +11,8 @@ closes.forEach(function(close){
         setTimeout(() => {
             location.href="../index.html";
         }, 1000);
-
     });
-
 })
-
 
 let cantar1 = document.querySelectorAll(".cantar1");
 cantar1.forEach(function(cantar1){
@@ -83,6 +80,146 @@ cantar5.forEach(function(cantar5){
         content.classList.add("zoomOut");
         setTimeout(() => {
             location.href="./song/alld.html";
+        }, 1000);
+    });
+})
+
+let cantar6 = document.querySelectorAll(".cantar6");
+cantar6.forEach(function(cantar6){
+    cantar6.addEventListener('click', function(ev6){
+        ev6.preventDefault();
+        content.classList.remove("animated");
+        content.classList.remove("zoomIn");
+        content.classList.add("animated");
+        content.classList.add("zoomOut");
+        setTimeout(() => {
+            location.href="./song/upl.html";
+        }, 1000);
+    });
+})
+
+let cantar7 = document.querySelectorAll(".cantar7");
+cantar7.forEach(function(cantar7){
+    cantar7.addEventListener('click', function(ev7){
+        ev7.preventDefault();
+        content.classList.remove("animated");
+        content.classList.remove("zoomIn");
+        content.classList.add("animated");
+        content.classList.add("zoomOut");
+        setTimeout(() => {
+            location.href="./song/caeiou.html";
+        }, 1000);
+    });
+})
+
+let cantar8 = document.querySelectorAll(".cantar8");
+cantar8.forEach(function(cantar8){
+    cantar8.addEventListener('click', function(ev8){
+        ev8.preventDefault();
+        content.classList.remove("animated");
+        content.classList.remove("zoomIn");
+        content.classList.add("animated");
+        content.classList.add("zoomOut");
+        setTimeout(() => {
+            location.href="./song/adv.html";
+        }, 1000);
+    });
+})
+
+let cantar9 = document.querySelectorAll(".cantar9");
+cantar9.forEach(function(cantar9){
+    cantar9.addEventListener('click', function(ev9){
+        ev9.preventDefault();
+        content.classList.remove("animated");
+        content.classList.remove("zoomIn");
+        content.classList.add("animated");
+        content.classList.add("zoomOut");
+        setTimeout(() => {
+            location.href="./song/sea.html";
+        }, 1000);
+    });
+})
+
+let cantar10 = document.querySelectorAll(".cantar10");
+cantar10.forEach(function(cantar10){
+    cantar10.addEventListener('click', function(ev10){
+        ev10.preventDefault();
+        content.classList.remove("animated");
+        content.classList.remove("zoomIn");
+        content.classList.add("animated");
+        content.classList.add("zoomOut");
+        setTimeout(() => {
+            location.href="./song/emm.html";
+        }, 1000);
+    });
+})
+
+let cantar11 = document.querySelectorAll(".cantar11");
+cantar11.forEach(function(cantar11){
+    cantar11.addEventListener('click', function(ev11){
+        ev11.preventDefault();
+        content.classList.remove("animated");
+        content.classList.remove("zoomIn");
+        content.classList.add("animated");
+        content.classList.add("zoomOut");
+        setTimeout(() => {
+            location.href="./song/elc.html";
+        }, 1000);
+    });
+})
+
+let cantar12 = document.querySelectorAll(".cantar12");
+cantar12.forEach(function(cantar12){
+    cantar12.addEventListener('click', function(ev12){
+        ev12.preventDefault();
+        content.classList.remove("animated");
+        content.classList.remove("zoomIn");
+        content.classList.add("animated");
+        content.classList.add("zoomOut");
+        setTimeout(() => {
+            location.href="./song/ipk.html";
+        }, 1000);
+    });
+})
+
+let cantar13 = document.querySelectorAll(".cantar13");
+cantar13.forEach(function(cantar13){
+    cantar13.addEventListener('click', function(ev13){
+        ev13.preventDefault();
+        content.classList.remove("animated");
+        content.classList.remove("zoomIn");
+        content.classList.add("animated");
+        content.classList.add("zoomOut");
+        setTimeout(() => {
+            location.href="./song/eneb.html";
+        }, 1000);
+    });
+})
+
+let cantar14 = document.querySelectorAll(".cantar14");
+cantar14.forEach(function(cantar14){
+    cantar14.addEventListener('click', function(ev14){
+        ev14.preventDefault();
+        content.classList.remove("animated");
+        content.classList.remove("zoomIn");
+        content.classList.add("animated");
+        content.classList.add("zoomOut");
+        setTimeout(() => {
+            location.href="./song/sta.html";
+        }, 1000);
+    });
+})
+
+let cantar15 = document.querySelectorAll(".cantar15");
+cantar15.forEach(function(cantar15){
+    cantar15.addEventListener('click', function(ev15){
+        ev15.preventDefault();
+        content.classList.remove("animated");
+        content.classList.remove("zoomIn");
+        content.classList.add("animated");
+        content.classList.add("zoomOut");
+        setTimeout(() => {
+            location.href="./song/miau.html";
         }, 1000);
     });
 })
